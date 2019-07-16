c049a365aa49d388991aaf9bb8629528443be4c1
takeshisean <takeshisean@hotmail.com>
2019-07-16 17:25:58 +0800

    Update matcher.go
    Updated packages from "s7techlab" to "takeshisean"

--- testing/expect/matcher.go
+++ testing/expect/matcher.go
@@ -8,7 +8,8 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
 	g "github.com/onsi/gomega"
-	"github.com/s7techlab/cckit/convert"
+	// "github.com/s7techlab/cckit/convert"
+	"github.com/takeshisean/cckit/convert"	
 )
 
 // ResponseOk expects peer.Response has shim.OK status and message has okSubstr prefix
