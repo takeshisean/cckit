9951e1a2d1bbed84b216a12a6cb8f4bd6eebc26f
takeshisean <takeshisean@hotmail.com>
2019-07-16 14:51:16 +0800

    Update main.go
    Updated package to "github.com/takeshisean/cckit/examples/erc20"

--- examples/erc20/bin/main.go
+++ examples/erc20/bin/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/s7techlab/cckit/examples/erc20"
+	// "github.com/s7techlab/cckit/examples/erc20"
+	"github.com/takeshisean/cckit/examples/erc20"
 )
 
 func main() {
