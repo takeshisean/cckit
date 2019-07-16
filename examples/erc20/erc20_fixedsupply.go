3dc43f554068a0878004656479c13292b58a1672
takeshisean <takeshisean@hotmail.com>
2019-07-16 16:34:22 +0800

    Update erc20_fixedsupply.go
    Updated package from "s7techlab" to "takeshisean"

--- examples/erc20/erc20_fixedsupply.go
+++ examples/erc20/erc20_fixedsupply.go
@@ -2,9 +2,13 @@ package erc20
 
 import (
 	"github.com/pkg/errors"
-	"github.com/s7techlab/cckit/extensions/owner"
-	"github.com/s7techlab/cckit/router"
-	p "github.com/s7techlab/cckit/router/param"
+	// "github.com/s7techlab/cckit/extensions/owner"
+	// "github.com/s7techlab/cckit/router"
+	// p "github.com/s7techlab/cckit/router/param"	
+	
+	"github.com/takeshisean/cckit/extensions/owner"
+	"github.com/takeshisean/cckit/router"
+	p "github.com/takeshisean/cckit/router/param"
 )
 
 const SymbolKey = `symbol`
