794da5ebf51ea5e5b9a1d6b7fe8c4e3d35ae683a
takeshisean <takeshisean@hotmail.com>
2019-07-16 17:24:54 +0800

    Update mockstub.go
    Updated packages from "s7techlab" to "takeshisean"

--- testing/mockstub.go
+++ testing/mockstub.go
@@ -13,7 +13,8 @@ import (
 	"github.com/hyperledger/fabric/protos/peer"
 	gologging "github.com/op/go-logging"
 	"github.com/pkg/errors"
-	"github.com/s7techlab/cckit/convert"
+	// "github.com/s7techlab/cckit/convert"
+	"github.com/takeshisean/cckit/convert"
 )
 
 const EventChannelBufferSize = 100
