287d52cc40a0fd6258372f33b509f03a2f8dd679
takeshisean <takeshisean@hotmail.com>
2019-07-16 16:28:50 +0800

    Update response.go
    Updated package from "s7techlab" to "takeshisean"

--- router/response.go
+++ router/response.go
@@ -3,7 +3,8 @@ package router
 import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
-	"github.com/s7techlab/cckit/response"
+	// "github.com/s7techlab/cckit/response"
+	"github.com/takeshisean/cckit/response"
 )
 
 // Response chaincode interface
